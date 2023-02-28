22a329ffa0f47b9c790fceb889ba53303e06ac56
Ellen Arteca <ellen.arteca@gmail.com>
2023-02-28 17:09:36 -0500

    solution to IP2 -- starter code

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -1,3 +1,9 @@
+export interface SBGame {
+  playerId: string,
+  score: number,
+  isOver: boolean,
+  timeLimit: number;
+};
 export type TownJoinResponse = {
   /** Unique ID that represents this player * */
   userID: string;
@@ -73,12 +79,6 @@ export interface ConversationArea {
   id: string;
   topic?: string;
   occupantsByID: string[];
-};
-export interface SBGame {
-  playerId: string,
-  score: number,
-  isOver: boolean,
-  timeLimit: number;
 };
 export interface BoundingBox {
   x: number;
