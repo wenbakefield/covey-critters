76f378ea45b1c4036a8487b591d2d4e67f4e6973
Ben Wakefield <wenbakefield@gmail.com>
2023-03-14 20:36:48 -0400

    tests for Pet model, fix for Species/MovementType

--- townService/src/lib/Player.ts
+++ townService/src/lib/Player.ts
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { IPet, Player as PlayerModel, PlayerLocation, TownEmitter } from '../types/CoveyTownSocket';
+import { Pet, Player as PlayerModel, PlayerLocation, TownEmitter } from '../types/CoveyTownSocket';
 
 /**
  * Each user who is connected to a town is represented by a Player object
@@ -23,7 +23,7 @@ export default class Player {
   /** A special town emitter that will emit events to the entire town BUT NOT to this player */
   public readonly townEmitter: TownEmitter;
 
-  private _pet?: IPet;
+  private _pet?: Pet;
 
   constructor(userName: string, townEmitter: TownEmitter) {
     this.location = {
@@ -58,11 +58,11 @@ export default class Player {
     return this._sessionToken;
   }
 
-  set pet(value: IPet | undefined) {
+  set pet(value: Pet | undefined) {
     this._pet = value;
   }
 
-  get pet(): IPet | undefined {
+  get pet(): Pet | undefined {
     return this._pet;
   }
 
