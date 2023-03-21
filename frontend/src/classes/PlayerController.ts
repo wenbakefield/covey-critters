41a28851a955606c74b6117d43bd483da52620c3
Jirawat Zhou <17247zhou@gmail.com>
2023-03-21 15:07:56 -0400

    Fix linter error and making pet optional parameter

--- frontend/src/classes/PlayerController.ts
+++ frontend/src/classes/PlayerController.ts
@@ -18,8 +18,8 @@ export type PlayerGameObjects = {
   label: Phaser.GameObjects.Text;
   locationManagedByGameScene: boolean /* For the local player, the game scene will calculate the current location, and we should NOT apply updates when we receive events */;
 
-  petSprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-  petLabel: Phaser.GameObjects.Text;
+  petSprite?: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  petLabel?: Phaser.GameObjects.Text;
 };
 export default class PlayerController extends (EventEmitter as new () => TypedEmitter<PlayerEvents>) {
   private _location: PlayerLocation;
@@ -90,7 +90,7 @@ export default class PlayerController extends (EventEmitter as new () => TypedEm
       label.setX(this.location.x);
       label.setY(this.location.y - 20);
 
-      if (this.pet !== undefined) {
+      if (this.pet !== undefined && petLabel && petSprite) {
         const { x, y } = this.pet.location;
         petSprite.setX(x);
         petSprite.setY(y);
@@ -101,12 +101,16 @@ export default class PlayerController extends (EventEmitter as new () => TypedEm
       // TODO: add different pet sprites
       if (this.location.moving) {
         sprite.anims.play(`misa-${this.location.rotation}-walk`, true);
-        petSprite.anims.play(`pet-${this.location.rotation}-walk`, true);
+        if (petSprite) {
+          petSprite.anims.play(`pet-${this.location.rotation}-walk`, true);
+        }
       } else {
         sprite.anims.stop();
-        petSprite.anims.stop();
         sprite.setTexture('atlas', `misa-${this.location.rotation}`);
-        petSprite.setTexture('pet', `pet-${this.location.rotation}`);
+        if (petSprite) {
+          petSprite.anims.stop();
+          petSprite.setTexture('pet', `pet-${this.location.rotation}`);
+        }
       }
     }
   }
