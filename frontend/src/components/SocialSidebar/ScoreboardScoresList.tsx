9e9fae3b1b8bf9fde049b7270ffde75a7c1e9527
gucere <gucer.e@northeastern.edu>
2023-03-22 19:12:18 -0400

    scoresList still needs update - not done!!

--- frontend/src/components/SocialSidebar/ScoreboardScoresList.tsx
+++ frontend/src/components/SocialSidebar/ScoreboardScoresList.tsx
@@ -3,24 +3,20 @@ import React from 'react';
 import ScoreboardController, {
   useScoreBoard,
 } from '../../classes/ScoreboardController';
-import { useActiveConversationAreas } from '../../classes/TownController';
 import PlayerName from './PlayerName';
+
+type ScoreBoardViewModel = {
+  scores: [PlayerModel, number][];
+};
 
 type ScoreBoardViewProps = {
   area: ScoreboardController;
 };
 
 /**
- * Displays a list of "active" conversation areas, along with their occupants
- *
- * A conversation area is "active" if its topic is not set to the constant NO_TOPIC_STRING that is exported from the ConverationArea file
- *
- * If there are active areas, it sorts them by label ascending
- *
- * See relevant hooks: useConversationAreas, usePlayersInTown.
+ * ScoreBoardList gets all scores and sends them to ScoreBoardView for creating and updating viewing panel.
  */
-function ScoreBoardView({ area }: ScoreBoardViewProps): JSX.Element {
-  const scores = useScoreBoard(area);
+function ScoreBoardView({ scores }: ScoreBoardViewModel): JSX.Element {
 
   return (
     <Box>
@@ -32,6 +28,7 @@ function ScoreBoardView({ area }: ScoreBoardViewProps): JSX.Element {
           return (
             //next pair of lines throw errors.
             <ListItem key={playerScoreTuple[0].id}>
+              <h4>{playerScoreTuple[1]}</h4>
               <PlayerName player={playerScoreTuple[0]} />
             </ListItem>
           );
@@ -40,21 +37,17 @@ function ScoreBoardView({ area }: ScoreBoardViewProps): JSX.Element {
     </Box>
   );
 }
-export default function ScoreBoardList(): JSX.Element {
-  const activeConversationAreas = useActiveConversationAreas();
+export default function ScoreBoardList({ area }: ScoreBoardViewProps): JSX.Element {
+  const scores = useScoreBoard(area);
   return (
     <Box>
       <Heading as='h2' fontSize='l'>
-        Active Conversation Areas:
+        Carnival Game Scoreboard:
       </Heading>
-      {activeConversationAreas.length === 0 ? (
-        <>No active conversation areas</>
+      {scores.length === 0 ? (
+        <>Scoreboard is empty</>
       ) : (
-        activeConversationAreas
-          .sort((a1, a2) =>
-            a1.id.localeCompare(a2.id, undefined, { numeric: true, sensitivity: 'base' }),
-          )
-          .map(area => <ConversationAreaView area={area} key={area.id} />)
+        <ScoreBoardView scoresList = {scores} />
       )}
     </Box>
   );
