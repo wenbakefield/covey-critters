f6499850493ae015ee065f01b6d40298098806b4
Jirawat Zhou <17247zhou@gmail.com>
2023-03-28 20:00:27 -0400

    FIx Socket Lag during player enters the game

--- frontend/src/components/Town/interactables/CarnivalGameAreaViewer.tsx
+++ frontend/src/components/Town/interactables/CarnivalGameAreaViewer.tsx
@@ -78,6 +78,7 @@ export function CarnivalGame({
     console.log(`play game is created ${game}`);
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     townController.emitGameOnTick('32');
     const updateGame = await townController.carnivalGameTimeLimitReach(controller);
     console.log(updateGame);
@@ -89,6 +90,8 @@ export function CarnivalGame({
 >>>>>>> bb0dd53 (Add Direction Component to Pet Model)
 =======
 >>>>>>> 1e7d72f (Add FollowPlayer Movement and adjust OffsetPlayer)
+=======
+>>>>>>> b55299c (FIx Socket Lag during player enters the game)
   }
 
   return (
