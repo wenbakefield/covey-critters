b792ed3e8501d229101d7c0ca087df6ea882710e
gucere <gucer.e@northeastern.edu>
2023-04-02 15:25:25 -0400

    TownsController.test.ts has to be fixed

--- frontend/src/classes/ScoreboardController.ts
+++ frontend/src/classes/ScoreboardController.ts
@@ -2,7 +2,7 @@ import EventEmitter from 'events';
 import _ from 'lodash';
 import { useEffect, useState } from 'react';
 import TypedEmitter from 'typed-emitter';
-import { Player as PlayerModel, PlayerScoreTuple } from '../types/CoveyTownSocket';
+import { PlayerScoreTuple } from '../types/CoveyTownSocket';
 
 export type ScoreBoardEvents = {
   scoreboardChange: (newScoreBoard: PlayerScoreTuple[]) => void;
@@ -23,65 +23,6 @@ export default class ScoreboardController extends (EventEmitter as new () => Typ
   set scoreboard(newScoreBoard: PlayerScoreTuple[]) {
     this._scoreboard = newScoreBoard;
     this.emit('scoreboardChange', newScoreBoard);
-  }
-
-  public getXScores(topNumber: number): PlayerScoreTuple[] {
-    if (topNumber < 1) {
-      return [];
-    }
-    if (this._scoreboard.length < topNumber) {
-      return this._scoreboard;
-    }
-    const topXOfScoreBoard: PlayerScoreTuple[] = [];
-    for (let i = 0; i < topNumber; i++) {
-      topXOfScoreBoard.push(this._scoreboard[i]);
-    }
-    return topXOfScoreBoard;
-  }
-
-  public removePlayer(player: PlayerModel): void {
-    const filteredList: PlayerScoreTuple[] = this._scoreboard.filter(
-      tuple => tuple.player.id !== player.id,
-    );
-    this._scoreboard = filteredList;
-    this.emit('scoreboardChange', filteredList);
-  }
-
-  public addPlayerScore(player: PlayerModel, score: number): void {
-    const newPair: PlayerScoreTuple = { player, score };
-    if (this._scoreboard.length === 0) {
-      this._scoreboard.push(newPair);
-      const newScoreBoard: PlayerScoreTuple[] = [newPair];
-      this.emit('scoreboardChange', newScoreBoard);
-    } else {
-      let ifPlaced = false;
-      const copyOfScoreBoard: PlayerScoreTuple[] = [];
-      for (let index = 0; index < this._scoreboard.length; index++) {
-        if (this._scoreboard[index].score < score && !ifPlaced) {
-          copyOfScoreBoard.push(newPair);
-          ifPlaced = true;
-        }
-        copyOfScoreBoard.push(this._scoreboard[index]);
-      }
-      if (!ifPlaced) {
-        copyOfScoreBoard.push(newPair);
-      }
-      this._scoreboard = copyOfScoreBoard;
-      this.emit('scoreboardChange', copyOfScoreBoard);
-    }
-  }
-
-  public getPercentile(score: number): number {
-    if (this._scoreboard.length === 0) {
-      return 0;
-    }
-    let betterScoreCount = 0;
-    this._scoreboard.forEach(tuple => {
-      if (tuple.score > score) {
-        betterScoreCount += 1;
-      }
-    });
-    return betterScoreCount / this._scoreboard.length;
   }
 }
 
