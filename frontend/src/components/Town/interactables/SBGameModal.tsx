b804c8162a4655e00b28ff25d9e375a32f6b73cc
Jirawat Zhou <17247zhou@gmail.com>
2023-04-03 11:51:38 -0400

    dispatch timeLimitReach when SBGame timer end

--- frontend/src/components/Town/interactables/SBGameModal.tsx
+++ frontend/src/components/Town/interactables/SBGameModal.tsx
@@ -10,7 +10,7 @@ import {
   Button,
 } from '@chakra-ui/react';
 import React, { useEffect, useRef, useState } from 'react';
-import {
+import TownController, {
   useCarnivalGameAreaController,
   useSpaceBarGameController,
 } from '../../../classes/TownController';
@@ -135,8 +135,12 @@ export default function SBGameModal({
 
 
   useEffect(() => {
-    if (count === sbGameController.scoreLimit || timeLeft === 0) {
+    async function terminateGame() {
+      await coveyTownController.carnivalGameTimeLimitReach(controller);
       setShowPopup(true);
+    }
+    if (count === sbGameController.scoreLimit || timeLeft === 0) {
+      terminateGame();
     }
   }, [count, sbGameController.scoreLimit, showPopup, timeLeft]);
 
