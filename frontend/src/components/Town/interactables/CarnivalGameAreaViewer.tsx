153b0c56ff0631338ec5957fce047651d3b764ff
gucere <97973553+gucere@users.noreply.github.com>
2023-04-02 23:44:30 -0400

    Copied out the dev version to fix merge conflict

--- frontend/src/components/Town/interactables/CarnivalGameAreaViewer.tsx
+++ frontend/src/components/Town/interactables/CarnivalGameAreaViewer.tsx
@@ -118,6 +118,7 @@ export function CarnivalGame({
             <PetPickerDialog isDisable={false} controller={controller} petName={'lemmy'} />
           </ModalFooter>
         }
+        {/* </form> */}
       </ModalContent>
     </Modal>
   );
